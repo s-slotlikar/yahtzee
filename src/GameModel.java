d89d90fa2c4024e6b261e3bdd129a0b7226c8a01
SnickerSatisfy <siddthekidlots@outlook.com>
2023-06-08 18:32:44 -0700

    Code Cleanup

---
+++ src/GameModel.java
@@ -0,0 +1,5 @@
+
+public class GameModel {
+	public static final int NUM_UPPER_SCORE_CATS = 6; 
+	public static final int NUM_LOWER_SCORE_CATS = 7;
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,11 +8,15 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Point;
+import java.awt.TextField;
+
 import javax.swing.JToggleButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class MainFrame extends JFrame {
 	
-	//States: May use this type of design
+	//UI State Machine Constants
 	public final static int RESET_GAME = 0;
 	public final static int BEFORE_FIRST_ROLL = 1;
 	public final static int BEFORE_SECOND_ROLL = 2;
@@ -21,21 +25,70 @@ public class MainFrame extends JFrame {
 	public final static int SCORING = 5;
 	public final static int GAME_OVER = 6;
 	
-	//Instance Variables
-	private JPanel contentPane;
-	private JToggleButton[] holdButtons;
+	// Lower Score 
+	public static final int THREE_OF_A_KIND = 0;
+	public static final int FOUR_OF_A_KIND = 1;
+	public static final int FULL_HOUSE = 2;
+	public static final int SMALL_STRAIGHT = 3;
+	public static final int LARGE_STRAIGHT = 4;
+	public static final int YAHTZEE = 5;
+	public static final int CHANCE = 6;
+	
+	//Global Constants
 	public static final int NUM_DICE = 5; 
 	public static final int NUM_SIDES = 6;
+	
+	//Instance Variables
 	private Dice myDice;
-	private boolean[] holdArray;
+	private GameModel game;
 	private int currentUIState;
-	private JToggleButton diceButton1;
-	private JToggleButton diceButton2;
-	private JToggleButton diceButton3;
-	private JToggleButton diceButton4;
-	private JToggleButton diceButton5;
-	private JButton scoreButton;
+	private int rollNum; //Temp
+	private JToggleButton[] diceToggleButtons;
+	private boolean[] holdArray;
+	private JToggleButton[] upperScoreButtonArray;
+	private JToggleButton[] lowerScoreButtonArray;
+	private JTextField[] upperScoreTextBoxArray;
+	private JTextField[] lowerScoreTextBoxArray;
+	
+	//Components
+	private JPanel contentPane;
+	private JToggleButton diceToggle1;
+	private JToggleButton diceToggle2;
+	private JToggleButton diceToggle3;
+	private JToggleButton diceToggle4;
+	private JToggleButton diceToggle6;
 	private JButton rollButton;
+	private JTextField aceTextField;
+	private JTextField twosTextField;
+	private JTextField threesTextField;
+	private JTextField foursTextField;
+	private JTextField fivesTextField;
+	private JTextField sixesTextField;
+	private JTextField threeOfAKindTextField;
+	private JTextField fourOfAKindTextField;
+	private JTextField fullHouseTextField;
+	private JTextField smallStraightTextField;
+	private JTextField largeStraightTextField;
+	private JTextField yahtzeeTextField;
+	private JTextField chanceTextField;
+	private JTextField bonusTextField;
+	private JTextField upperScoreTextField;
+	private JTextField lowerScoreTextField;
+	private JTextField grandTotalTextField;
+	private JToggleButton aceToggle;
+	private JToggleButton twosToggle;
+	private JToggleButton threesToggle;
+	private JToggleButton foursToggle;
+	private JToggleButton fivesToggle;
+	private JToggleButton sixesToggle;
+	private JToggleButton threeOfAKindToggle;
+	private JToggleButton fourOfAKindToggle;
+	private JToggleButton fullHouseToggle;
+	private JToggleButton smallStraightToggle;
+	private JToggleButton largeStraightToggle;
+	private JToggleButton yahtzeeToggle;
+	private JToggleButton chanceToggle;
+	
 
 	/**
 	 * Launch the application.
@@ -59,134 +112,120 @@ public class MainFrame extends JFrame {
 	public MainFrame() {
 		/* UI Components */
 		initComponents();
+		/* Initialization of Instance Variables */
+		
+		//Initializes the set of dices
+		myDice = new Dice(NUM_DICE, NUM_SIDES);		
+		//Initialize an array of buttons for each die
+		diceToggleButtons = new JToggleButton[NUM_DICE];
+		diceToggleButtons[0] = diceToggle1;
+		diceToggleButtons[1] = diceToggle2;
+		diceToggleButtons[2] = diceToggle3;
+		diceToggleButtons[3] = diceToggle4;
+		diceToggleButtons[4] = diceToggle6;
+		
+		upperScoreButtonArray = new JToggleButton[GameModel.NUM_UPPER_SCORE_CATS + 1];
+		upperScoreButtonArray[1] = this.aceToggle;
+		upperScoreButtonArray[2] = this.twosToggle;
+		upperScoreButtonArray[3] = this.threesToggle;
+		upperScoreButtonArray[4] = this.foursToggle;
+		upperScoreButtonArray[5] = this.fivesToggle;
+		upperScoreButtonArray[6] = this.sixesToggle;
+		
+		lowerScoreButtonArray = new JToggleButton[GameModel.NUM_LOWER_SCORE_CATS];
+		lowerScoreButtonArray[THREE_OF_A_KIND] = this.threeOfAKindToggle;
+		
+		lowerScoreTextBoxArray = new JTextField[GameModel.NUM_LOWER_SCORE_CATS];
+		lowerScoreTextBoxArray[THREE_OF_A_KIND] = this.threeOfAKindTextField;
+		
+		
+		holdArray = new boolean[NUM_DICE];
+		
+		game = new GameModel();
+		
+		
+		manageUIState(RESET_GAME);
+		
 		//Temp - should be RESET_GAME later
 		manageUIState(BEFORE_FIRST_ROLL);
-		
-		/* Event Handlers */
-		
-		//Score Button 
-		scoreButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				manageUIState(BEFORE_FIRST_ROLL);
-			}
-		});
+		rollNum = 1;
 		
 		//Roll Button
 		rollButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {				
+			public void actionPerformed(ActionEvent e) {
+				
+				int rollValue;
+				
 				for (int i = 0; i < NUM_DICE; i++) { 
+					
 					// If the user is not holding this die then roll
 					if (!holdArray[i]) {
-						myDice.rollDie(i);
-						holdButtons[i].setText("" + myDice.getDieValue(i));
+						
+						rollValue = myDice.rollDie(i);
+						diceToggleButtons[i].setText("" + rollValue);
+						
 					}
 				}
-				if (currentUIState == BEFORE_FIRST_ROLL) {
+				rollNum++;
+				
+				switch(currentUIState) {
+				case BEFORE_FIRST_ROLL:
+					
 					manageUIState(BEFORE_SECOND_ROLL);
-				} else if (currentUIState == BEFORE_SECOND_ROLL) {
+					
+					break;
+				case BEFORE_SECOND_ROLL:
+					
 					manageUIState(BEFORE_THIRD_ROLL);
-				} else if (currentUIState == BEFORE_THIRD_ROLL) {
+					
+					break;
+				case BEFORE_THIRD_ROLL:
+					
 					manageUIState(AFTER_THIRD_ROLL);
+					
+					break;
+				}
+				
+				if (rollNum > 3) {
+					rollButton.setEnabled(true);
 				}
 			}
 		});
 		
 		//Dice 1
-		diceButton1.addActionListener(new ActionListener() {
+		diceToggle1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				holdArray[0] = !holdArray[0];
 			}
 		});
 		
 		//Dice 2
-		diceButton2.addActionListener(new ActionListener() {
+		diceToggle2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				holdArray[1] = !holdArray[1];
 			}
 		});
 		
 		//Dice 3
-		diceButton3.addActionListener(new ActionListener() {
+		diceToggle3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				holdArray[2] = !holdArray[2];
 			}
 		});
 		
 		//Dice 4
-		diceButton4.addActionListener(new ActionListener() {
+		diceToggle4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				holdArray[3] = !holdArray[3];
 			}
 		});
 		
 		//Dice 5
-		diceButton5.addActionListener(new ActionListener() {
+		diceToggle6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				holdArray[4] = !holdArray[4];
 			}
 		});
-	}
-	public void initComponents() {
-		//Panel
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 483, 455);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		//Dice 1
-		diceButton1 = new JToggleButton("");
-		diceButton1.setSize(82, 82);
-		diceButton1.setLocation(new Point(10, 10));
-		contentPane.setLayout(null);
-		contentPane.add(diceButton1);
-		
-		//Dice2
-		diceButton2 = new JToggleButton("");
-		diceButton2.setLocation(new Point(10, 10));
-		diceButton2.setBounds(103, 10, 82, 82);
-		contentPane.add(diceButton2);
-		
-		//Dice3
-		diceButton3 = new JToggleButton("");
-		diceButton3.setLocation(new Point(10, 10));
-		diceButton3.setBounds(195, 10, 82, 82);
-		contentPane.add(diceButton3);
-		
-		//Dice4
-		diceButton4 = new JToggleButton("");
-		diceButton4.setLocation(new Point(10, 10));
-		diceButton4.setBounds(287, 10, 82, 82);
-		contentPane.add(diceButton4);
-			
-		//Dice5
-		diceButton5 = new JToggleButton("");
-		diceButton5.setLocation(new Point(10, 10));
-		diceButton5.setBounds(379, 10, 82, 82);
-		contentPane.add(diceButton5);
-		
-		//Score Button
-		scoreButton = new JButton("Score");
-		scoreButton.setBounds(379, 144, 82, 20);
-		contentPane.add(scoreButton);
-		
-		//Roll Button
-		rollButton = new JButton("Roll");
-		rollButton.setBounds(195, 136, 82, 36);
-		contentPane.add(rollButton);
-			
-		/* Initialization of Instance Variables */
-				
-		//Initializes the set of dices
-		myDice = new Dice(NUM_DICE, NUM_SIDES);		
-		//Initialize an array of buttons for each die
-		holdButtons = new JToggleButton[NUM_DICE];
-		holdButtons[0] = diceButton1;
-		holdButtons[1] = diceButton2;
-		holdButtons[2] = diceButton3;
-		holdButtons[3] = diceButton4;
-		holdButtons[4] = diceButton5;
-		holdArray = new boolean[NUM_DICE];
 	}
 	
 	//Util Methods
@@ -195,7 +234,9 @@ public class MainFrame extends JFrame {
 			case RESET_GAME:
 				break; 
 			case BEFORE_FIRST_ROLL:
-				System.out.println("CASE 1");
+				//Temp
+				rollButton.setEnabled(true);
+				
 				//Disable the roll buttons
 				clearAndDisableHoldButtons();
 				
@@ -204,28 +245,31 @@ public class MainFrame extends JFrame {
 			
 				//Clear the hold array
 				clearHoldArray();
+				
 				break;
 			case BEFORE_SECOND_ROLL:
 				//Enables the hold buttons after rolling the dice once
-				System.out.println("CASE 2");
-				enableHoldButtons();
+				setHoldButtonsEnableState(true);
+				
 				break;
 			case BEFORE_THIRD_ROLL:
-				System.out.println("CASE 3");
+				
 				break;
 			case AFTER_THIRD_ROLL:
-				System.out.println("CASE 4");
 				//Disable the Roll Button
+				setHoldButtonsEnableState(false);
 				rollButton.setEnabled(false);
+				
+				break;
 			case SCORING:
-				System.out.println("CASE 5");
+				
 				break;
 			case GAME_OVER:
-				System.out.println("CASE 6");
+				
 				break;
 			default:
-				System.out.println("CASE 7");
 				JOptionPane.showMessageDialog(this, "Invalid UI state detected");
+				
 				break;
 		}
 		currentUIState = nextState;
@@ -233,11 +277,11 @@ public class MainFrame extends JFrame {
 	
 	//Helper Methods
 	private void clearAndDisableHoldButtons() {
-		for (int i = 0; i < holdButtons.length; i++) {
+		for (int i = 0; i < diceToggleButtons.length; i++) {
 			System.out.println("disabled button");
-			holdButtons[i].setText("");
-			holdButtons[i].setEnabled(false);
-			holdButtons[i].setSelected(false);
+			diceToggleButtons[i].setText("");
+			diceToggleButtons[i].setEnabled(false);
+			diceToggleButtons[i].setSelected(false);
 		}
 	}
 	
@@ -248,10 +292,210 @@ public class MainFrame extends JFrame {
 		}
 	}
 	
-	private void enableHoldButtons() {
+	private void setHoldButtonsEnableState(boolean isEnabled) {
 		System.out.println("enabled button");
-		for (int i = 0; i < holdButtons.length; i++) {
-			holdButtons[i].setEnabled(true);
+		for (int i = 0; i < diceToggleButtons.length; i++) {
+			diceToggleButtons[i].setEnabled(isEnabled);
 		}
+	}
+	
+	//All the UI Components setup here
+	public void initComponents() {
+		//Panel
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 510, 655);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		//Dice 1
+		diceToggle1 = new JToggleButton("");
+		diceToggle1.setSize(82, 82);
+		diceToggle1.setLocation(new Point(10, 10));
+		contentPane.setLayout(null);
+		contentPane.add(diceToggle1);
+		
+		//Dice2
+		diceToggle2 = new JToggleButton("");
+		diceToggle2.setLocation(new Point(10, 10));
+		diceToggle2.setBounds(110, 10, 82, 82);
+		contentPane.add(diceToggle2);
+		
+		//Dice3
+		diceToggle3 = new JToggleButton("");
+		diceToggle3.setLocation(new Point(10, 10));
+		diceToggle3.setBounds(210, 10, 82, 82);
+		contentPane.add(diceToggle3);
+		
+		//Dice4
+		diceToggle4 = new JToggleButton("");
+		diceToggle4.setLocation(new Point(10, 10));
+		diceToggle4.setBounds(310, 10, 82, 82);
+		contentPane.add(diceToggle4);
+			
+		//Dice5
+		diceToggle6 = new JToggleButton("");
+		diceToggle6.setLocation(new Point(10, 10));
+		diceToggle6.setBounds(410, 10, 82, 82);
+		contentPane.add(diceToggle6);
+		
+		//Roll Button
+		rollButton = new JButton("Roll");
+		rollButton.setBounds(181, 136, 96, 42);
+		contentPane.add(rollButton);
+		
+		aceToggle = new JToggleButton("Ace");
+		aceToggle.setBounds(10, 206, 82, 34);
+		contentPane.add(aceToggle);
+		
+		twosToggle = new JToggleButton("Twos");
+		twosToggle.setBounds(10, 251, 82, 34);
+		contentPane.add(twosToggle);
+		
+		threesToggle = new JToggleButton("Threes");
+		threesToggle.setBounds(10, 296, 82, 34);
+		contentPane.add(threesToggle);
+		
+		foursToggle = new JToggleButton("Fours");
+		foursToggle.setBounds(10, 341, 82, 34);
+		contentPane.add(foursToggle);
+		
+		fivesToggle = new JToggleButton("Fives");
+		fivesToggle.setBounds(10, 386, 82, 34);
+		contentPane.add(fivesToggle);
+		
+		sixesToggle = new JToggleButton("Sixes");
+		sixesToggle.setBounds(10, 431, 82, 34);
+		contentPane.add(sixesToggle);
+		
+		threeOfAKindToggle = new JToggleButton("3 of a Kind");
+		threeOfAKindToggle.setBounds(236, 206, 119, 34);
+		contentPane.add(threeOfAKindToggle);
+		
+		fourOfAKindToggle = new JToggleButton("4 of a Kind");
+		fourOfAKindToggle.setBounds(236, 251, 119, 34);
+		contentPane.add(fourOfAKindToggle);
+		
+		fullHouseToggle = new JToggleButton("Full House");
+		fullHouseToggle.setBounds(236, 296, 119, 34);
+		contentPane.add(fullHouseToggle);
+		
+		smallStraightToggle = new JToggleButton("Small Straight");
+		smallStraightToggle.setBounds(236, 341, 119, 34);
+		contentPane.add(smallStraightToggle);
+		
+		largeStraightToggle = new JToggleButton("Large Straight");
+		largeStraightToggle.setBounds(236, 386, 119, 34);
+		contentPane.add(largeStraightToggle);
+		
+		yahtzeeToggle = new JToggleButton("YAHTZEE");
+		yahtzeeToggle.setBounds(236, 431, 119, 34);
+		contentPane.add(yahtzeeToggle);
+		
+		chanceToggle = new JToggleButton("Chance");
+		chanceToggle.setBounds(236, 475, 119, 34);
+		contentPane.add(chanceToggle);
+		
+		aceTextField = new JTextField();
+		aceTextField.setBounds(132, 214, 53, 19);
+		contentPane.add(aceTextField);
+		aceTextField.setColumns(10);
+		
+		twosTextField = new JTextField();
+		twosTextField.setBounds(132, 259, 53, 19);
+		contentPane.add(twosTextField);
+		twosTextField.setColumns(10);
+		
+		threesTextField = new JTextField();
+		threesTextField.setColumns(10);
+		threesTextField.setBounds(132, 304, 53, 19);
+		contentPane.add(threesTextField);
+		
+		foursTextField = new JTextField();
+		foursTextField.setColumns(10);
+		foursTextField.setBounds(132, 349, 53, 19);
+		contentPane.add(foursTextField);
+		
+		fivesTextField = new JTextField();
+		fivesTextField.setColumns(10);
+		fivesTextField.setBounds(132, 394, 53, 19);
+		contentPane.add(fivesTextField);
+		
+		sixesTextField = new JTextField();
+		sixesTextField.setColumns(10);
+		sixesTextField.setBounds(132, 439, 53, 19);
+		contentPane.add(sixesTextField);
+		
+		threeOfAKindTextField = new JTextField();
+		threeOfAKindTextField.setColumns(10);
+		threeOfAKindTextField.setBounds(379, 214, 59, 19);
+		contentPane.add(threeOfAKindTextField);
+		
+		fourOfAKindTextField = new JTextField();
+		fourOfAKindTextField.setColumns(10);
+		fourOfAKindTextField.setBounds(379, 259, 59, 19);
+		contentPane.add(fourOfAKindTextField);
+		
+		fullHouseTextField = new JTextField();
+		fullHouseTextField.setColumns(10);
+		fullHouseTextField.setBounds(379, 304, 59, 19);
+		contentPane.add(fullHouseTextField);
+		
+		smallStraightTextField = new JTextField();
+		smallStraightTextField.setColumns(10);
+		smallStraightTextField.setBounds(379, 349, 59, 19);
+		contentPane.add(smallStraightTextField);
+		
+		largeStraightTextField = new JTextField();
+		largeStraightTextField.setColumns(10);
+		largeStraightTextField.setBounds(379, 394, 59, 19);
+		contentPane.add(largeStraightTextField);
+		
+		yahtzeeTextField = new JTextField();
+		yahtzeeTextField.setColumns(10);
+		yahtzeeTextField.setBounds(379, 439, 59, 19);
+		contentPane.add(yahtzeeTextField);
+		
+		chanceTextField = new JTextField();
+		chanceTextField.setColumns(10);
+		chanceTextField.setBounds(379, 483, 59, 19);
+		contentPane.add(chanceTextField);
+		
+		bonusTextField = new JTextField();
+		bonusTextField.setColumns(10);
+		bonusTextField.setBounds(132, 477, 53, 19);
+		contentPane.add(bonusTextField);
+		
+		upperScoreTextField = new JTextField();
+		upperScoreTextField.setColumns(10);
+		upperScoreTextField.setBounds(132, 505, 53, 19);
+		contentPane.add(upperScoreTextField);
+		
+		lowerScoreTextField = new JTextField();
+		lowerScoreTextField.setColumns(10);
+		lowerScoreTextField.setBounds(379, 522, 59, 19);
+		contentPane.add(lowerScoreTextField);
+		
+		grandTotalTextField = new JTextField();
+		grandTotalTextField.setColumns(10);
+		grandTotalTextField.setBounds(379, 551, 59, 19);
+		contentPane.add(grandTotalTextField);
+		
+		JLabel bonusLabel = new JLabel("Bonus if > 63");
+		bonusLabel.setBounds(20, 475, 87, 23);
+		contentPane.add(bonusLabel);
+		
+		JLabel totalUpperScoreLabel = new JLabel("Upper Score");
+		totalUpperScoreLabel.setBounds(20, 503, 127, 23);
+		contentPane.add(totalUpperScoreLabel);
+		
+		JLabel lowerScoreLabel = new JLabel("Lower Score");
+		lowerScoreLabel.setBounds(267, 528, 102, 13);
+		contentPane.add(lowerScoreLabel);
+		
+		JLabel grandTotalLabel = new JLabel("Grand Total");
+		grandTotalLabel.setBounds(267, 557, 102, 13);
+		contentPane.add(grandTotalLabel);
+		holdArray = new boolean[NUM_DICE];
 	}
 }
